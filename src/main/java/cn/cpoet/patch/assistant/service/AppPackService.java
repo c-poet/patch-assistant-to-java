89de18f3622feb5da2a907a1b12e84f08c65bc65
CPoet <llzero54@foxmail.com>
2025-05-22 18:17:04 +0800

    fix: 处理应用包生产失败问题

--- src/main/java/cn/cpoet/patch/assistant/service/AppPackService.java
+++ src/main/java/cn/cpoet/patch/assistant/service/AppPackService.java
@@ -1,15 +1,16 @@
 package cn.cpoet.patch.assistant.service;
 
+import cn.cpoet.patch.assistant.constant.FileExtConst;
 import cn.cpoet.patch.assistant.core.AppContext;
 import cn.cpoet.patch.assistant.exception.AppException;
-import cn.cpoet.patch.assistant.view.tree.FileNode;
-import cn.cpoet.patch.assistant.view.tree.TreeInfo;
-import cn.cpoet.patch.assistant.view.tree.TreeNode;
-import cn.cpoet.patch.assistant.view.tree.ZipEntryNode;
+import cn.cpoet.patch.assistant.view.tree.*;
 
 import java.io.*;
+import java.time.LocalDateTime;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 /**
  * 应用包处理
@@ -50,36 +51,76 @@ public class AppPackService extends BasePackService {
     public void savePack(File file, TreeInfo appTree) {
         TreeNode rootNode = appTree.getRootNode();
         try (OutputStream out = new FileOutputStream(file);
-             ZipOutputStream zipOut = new ZipOutputStream(out)) {
+             JarOutputStream jarOut = new JarOutputStream(out)) {
             if (rootNode.getChildren() != null) {
-                rootNode.getChildren().forEach(node -> writeTreeNode2Pack(zipOut, node));
+                for (TreeNode child : rootNode.getChildren()) {
+                    writeTreeNode2Pack(jarOut, (ZipEntryNode) child);
+                }
             }
         } catch (Exception e) {
             throw new AppException("写入应用包失败", e);
         }
     }
 
-    private void writeTreeNode2Pack(ZipOutputStream zipOut, TreeNode node) {
-        TreeNode targetNode = node;
-        if (node.getMappedNode() != null) {
-            targetNode = node.getMappedNode();
-            if (!Boolean.TRUE.equals(targetNode.getChecked())) {
-                return;
-            }
+    protected void writeTreeNode2Pack(JarOutputStream jarOut, ZipEntryNode node) throws IOException {
+        if (!node.isDir() && node.getName().endsWith(FileExtConst.DOT_JAR)) {
+            writeTreeNode2PackWithJar(jarOut, node);
+            return;
         }
-        try {
-            if (targetNode instanceof ZipEntryNode) {
-                ZipEntryNode zipEntryNode = (ZipEntryNode) targetNode;
-                zipOut.putNextEntry(zipEntryNode.getEntry());
-                if (!zipEntryNode.getEntry().isDirectory()) {
-                    zipOut.write(zipEntryNode.getBytes());
-                }
+        if (node.getMappedNode() == null) {
+            jarOut.putNextEntry(getJarEntryWithZipEntry(node.getEntry()));
+            if (!node.isDir()) {
+                jarOut.write(node.getBytes());
             }
-        } catch (Exception e) {
-            throw new AppException("写入压缩文件失败", e);
+        } else {
+            TreeKindNode mappedNode = (TreeKindNode) node.getMappedNode();
+            JarEntry jarEntry = getJarEntryWithZipEntry(node.getEntry());
+            jarEntry.setTimeLocal(mappedNode.getModifyTime());
+            jarOut.putNextEntry(jarEntry);
+            if (!jarEntry.isDirectory()) {
+                jarOut.write(mappedNode.getBytes());
+            }
         }
         if (node.getChildren() != null) {
-            node.getChildren().forEach(child -> writeTreeNode2Pack(zipOut, child));
+            for (TreeNode child : node.getChildren()) {
+                writeTreeNode2Pack(jarOut, (ZipEntryNode) child);
+            }
         }
+    }
+
+    protected void writeTreeNode2PackWithJar(JarOutputStream jarOut, ZipEntryNode node) throws IOException {
+        if (node.getChildren() == null || node.getChildren().isEmpty()) {
+            jarOut.putNextEntry(getJarEntryWithZipEntry(node.getEntry()));
+            jarOut.write(node.getBytes());
+            return;
+        }
+        byte[] bytes = getBytesWithJarNode(node);
+        JarEntry jarEntry = getJarEntryWithZipEntry(node.getEntry());
+        jarEntry.setTimeLocal(LocalDateTime.now());
+        jarOut.putNextEntry(jarEntry);
+        jarOut.write(bytes);
+    }
+
+    protected byte[] getBytesWithJarNode(ZipEntryNode jarNode) throws IOException {
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+             JarOutputStream jarOut = new JarOutputStream(out)) {
+            for (TreeNode child : jarNode.getChildren()) {
+                writeTreeNode2Pack(jarOut, (ZipEntryNode) child);
+            }
+            return out.toByteArray();
+        }
+    }
+
+    protected JarEntry getJarEntryWithZipEntry(ZipEntry zipEntry) {
+        JarEntry jarEntry = new JarEntry(zipEntry.getName());
+        jarEntry.setComment(zipEntry.getComment());
+        if (zipEntry.getCreationTime() != null) {
+            jarEntry.setCreationTime(zipEntry.getCreationTime());
+        }
+        if (zipEntry.getLastAccessTime() != null) {
+            jarEntry.setLastAccessTime(zipEntry.getLastAccessTime());
+        }
+        jarEntry.setLastModifiedTime(zipEntry.getLastModifiedTime());
+        return jarEntry;
     }
 }
